323e3d0eef5bbfd3a65be6351437809d100ecbb0
glongh <glongh@gmail.com>
2019-11-26 08:21:55 -0500

    Updates

---
+++ precache-manifest.41b46caa02abd0b86565454e395c2fee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c0f9aade0a9dd2644dcd7bed23d15a",
+    "url": "/@react-corekit/use-idle/index.html"
+  },
+  {
+    "revision": "1f70ed31e7945848a5af",
+    "url": "/@react-corekit/use-idle/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "8ecb0d75b57cf7d1dca5",
+    "url": "/@react-corekit/use-idle/static/js/2.03d91ed5.chunk.js"
+  },
+  {
+    "revision": "1f70ed31e7945848a5af",
+    "url": "/@react-corekit/use-idle/static/js/main.34dc6392.chunk.js"
+  },
+  {
+    "revision": "14f72aefe71ff675a2ab",
+    "url": "/@react-corekit/use-idle/static/js/runtime-main.33af30df.js"
+  }
+]);
