929c9e0264e7016e686bc4e55d0adfecea7ae8f6
glongh <glongh@gmail.com>
2019-12-04 09:08:36 -0500

    doc(Example): added mousemove event

--- example/src/App.js
+++ example/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useIdle } from "@react-corekit/use-idle";
 
 const App = () => {
-  const isIdle = useIdle({ timeToIdle: 1000 });
+  const isIdle = useIdle({ timeToIdle: 1000, ignoredEventsWhenIdle: [] });
   return <div>{isIdle ? "Are you still there?" : "Yes, you are here"}</div>;
 };
 export default App;
